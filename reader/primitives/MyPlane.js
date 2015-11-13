86de35f975ce08bda1b2355f260f5d936f331e65
sceptross <afonso.castro11@gmail.com>
2015-11-13 13:05:24 +0000

    Done MyPlane.js

---
+++ reader/primitives/MyPlane.js
@@ -0,0 +1,13 @@
+function MyPlane(scene, parts){
+	var nurbsSurface = new CGFnurbsSurface(1, 1, [0,0,1,1], [0,0,1,1], [[[0,0,0],[1,0,0]], [[1,0,0], [1,0,1]]]);
+	getSurfacePoint = function(u, v) {
+		return nurbsSurface.getPoint(u, v);
+	};
+
+    CGFnurbsObject.call(this,scene, getSurfacePoint, parts, parts);
+}
+
+MyPlane.prototype = Object.create(CGFnurbsObject.prototype);
+MyPlane.prototype.constructor = MyPlane;
+
+MyPlane.prototype.scaleTexCoords = function(ampS, ampT) {}
